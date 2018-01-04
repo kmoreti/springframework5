933a58e9716431a9764ebc5eb1fbe7400aa748a4
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-01-04 00:11:19 -0200

    Finished images handling.

--- spring5-recipe-app/src/main/java/moreti/springframework/spring5recipeapp/controllers/ImageController.java
+++ spring5-recipe-app/src/main/java/moreti/springframework/spring5recipeapp/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package moreti.springframework.spring5recipeapp.controllers;
 
+import moreti.springframework.spring5recipeapp.commands.RecipeCommand;
 import moreti.springframework.spring5recipeapp.services.ImageService;
 import moreti.springframework.spring5recipeapp.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -31,8 +38,22 @@ public class ImageController {
     @GetMapping("/recipe/{id}/image")
     public String showUploadForm(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-
         return "recipe/imageuploadform";
     }
 
+    @GetMapping("/recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand command = recipeService.findCommandById(Long.valueOf(id));
+
+        byte [] byteArray = new byte[command.getImage().length];
+
+        int i = 0;
+
+        for (byte b: command.getImage()) {
+            byteArray[i++] = b;
+        }
+        response.setContentType("image/jpeg");
+        InputStream inputStream = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(inputStream,response.getOutputStream());
+    }
 }
--- spring5-recipe-app/src/main/java/moreti/springframework/spring5recipeapp/services/ImageServiceImpl.java
+++ spring5-recipe-app/src/main/java/moreti/springframework/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,14 +1,49 @@
 package moreti.springframework.spring5recipeapp.services;
 
 import lombok.extern.slf4j.Slf4j;
+import moreti.springframework.spring5recipeapp.domain.Recipe;
+import moreti.springframework.spring5recipeapp.repository.RecipeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
-    public void saveImageFile(Long aLong, MultipartFile file) {
-        log.error("Received a file.");
+    @Transactional
+    public void saveImageFile(Long id, MultipartFile file) {
+        log.info("Received a file.");
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (!recipeOptional.isPresent()) {
+            log.error("Recipe not found. Id: " + id);
+        } else {
+            try {
+                Recipe recipe = recipeOptional.get();
+                Byte [] byteObjects = new Byte[file.getBytes().length];
+
+                int i = 0;
+                for (byte b : file.getBytes()) {
+                    byteObjects[i++] = b;
+                }
+                recipe.setImage(byteObjects);
+                recipeRepository.save(recipe);
+            } catch (IOException e) {
+                //todo handle better
+                log.error("An error occurred.", e);
+                e.printStackTrace();
+            }
+        }
     }
 }
